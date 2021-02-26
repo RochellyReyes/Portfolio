9f4a72fc464412c0bb847e6d6ae6fd36d64eb63e
Rochelly Reyes <rochelly.rhyna@gmail.com>
2021-02-26 14:18:07 -0500

    resume open new tab

--- src/Views/Resume.jsx
+++ src/Views/Resume.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import Footer from "../Components/Footer";
 import "../styling/resume.css";
 
-const Resume = () => {
 
+const Resume = () => {
 
     return(
         <div>
-            <a href="https://drive.google.com/file/d/1Odv5lSmPQmA41xBuxxs_FWUgJ1wjWBvC/view?usp=sharing">
-
-                <img className="resumeImg" src="/images/resume/Portfolio-Red.jpg" alt="Rochelly's Resume"/>
-            </a>
+            <img className="resumeImg" src="/images/resume/Portfolio-Red.jpg" alt="Rochelly's Resume" 
+            onClick={()=> window.open(`https://drive.google.com/file/d/1Odv5lSmPQmA41xBuxxs_FWUgJ1wjWBvC/view?usp=sharing`, `_blank`)}
+            />
             
             <p className="resumeText"> Click on the resume to be taken to a PDF downloadable version</p>
             <Footer/>
